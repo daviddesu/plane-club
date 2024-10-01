5eba87b04d6939ededdd548af8cf21efff1961eb
davidlee <d.c.s.lee-09@alumni.lboro.ac.uk>
2024-10-01 12:59:44 +0100

    added posthog and sentry

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use PostHog\PostHog;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        PostHog::init(
+            'phc_9P4bssfAQEZUcYEYjvQFrRcS3ApwrNTMDPQySGFlqAl',
+            [
+                'host' => 'https://us.i.posthog.com'
+            ]
+        );
     }
 }
