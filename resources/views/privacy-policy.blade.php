04dc90793d8d7e98c5631988865bff9b140e5b7e
davidlee <d.c.s.lee-09@alumni.lboro.ac.uk>
2025-02-11 16:10:51 +0000

    Updated legal

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
     <div name="termly-embed" data-id="f18e572b-567a-4704-b52c-0b6fdd7d9ab6"></div>
-<script type="text/javascript">(function(d, s, id) {
-  var js, tjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "https://app.termly.io/embed-policy.min.js";
-  tjs.parentNode.insertBefore(js, tjs);
-}(document, 'script', 'termly-jssdk'));</script>
+    <script type="text/javascript">(function(d, s, id) {
+      var js, tjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "https://app.termly.io/embed-policy.min.js";
+      tjs.parentNode.insertBefore(js, tjs);
+    }(document, 'script', 'termly-jssdk'));</script>
 </x-app-layout>
